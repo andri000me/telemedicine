9bc83400bec3f83a6ea1c0295bea500c584ce2f0
Codavlo Indonesia <srv.codavlo@gmail.com>
2020-06-22 10:57:52 +0700

    Change primary color

--- application/views/common/login.php
+++ application/views/common/login.php
@@ -33,7 +33,7 @@
          <div class="container">
             <div class="row align-items-center">
                <div class="col-11 col-xl-2">
-                  <h1 class="mb-0 site-logo"><a href="<?= base_url() ?>" class="text-black h2 mb-0">Telemedicine</a></h1>
+                  <a href="<?= base_url() ?>" class="text-black h2 mb-0"><img src="<?= base_url('assets/landing/images/logo_kariadi.png') ?>" alt="logo_kariadi" class="float-left" width="50%"> Telemedicine
                </div>
                <div class="col-12 col-md-10 d-none d-xl-block">
                   <nav class="site-navigation position-relative text-right" role="navigation">
@@ -42,7 +42,7 @@
                         <!-- <li><a href="http://perjanjian.rskariadi.id/Admission/JadwalDokter" target="_blank">Daftar Online</a></li>
                         <li><a href="http://perjanjian.rskariadi.id/Admission/AdmissionCheckIn" target="_blank">Cek Bukti Pendaftaran </a></li> -->
                         <li><a href="<?= base_url('register') ?>">Daftar Online</a></li>
-                        <li class="active"><a href="<?= base_url('online_checking') ?>">Cek Bukti Pendaftaran </a></li>
+                        <li class=""><a href="<?= base_url('online_checking') ?>">Cek Bukti Pendaftaran </a></li>
                         <li><a href="<?= base_url('guide') ?>">Panduan Pengguna</a></li>
                      </ul>
                   </nav>
